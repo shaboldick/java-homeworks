7a60eaded2c6b9c2117174845f5e34307254ceb4
shaboldick <shaboldick@gmail.com>
2025-10-21 04:27:11 +0300

    Lesson 4

---
+++ src/main/java/ru/qa/lesson/lesson_4/Dog.java
@@ -0,0 +1,32 @@
+package ru.qa.lesson.lesson_4;
+
+public class Dog extends Animal {
+    private static int count = 0;
+
+    public Dog(String name) {
+        super(name, 500, 10);
+        count++;
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance <= maxRunDistance) {
+            System.out.println(name + " пробежал " + distance + " м.");
+        } else {
+            System.out.println(name + " не может пробежать " + distance + " м.");
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance <= maxSwimDistance) {
+            System.out.println(name + " проплыл " + distance + " м.");
+        } else {
+            System.out.println(name + " не может проплыть " + distance + " м.");
+        }
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
